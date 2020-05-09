3dd0fb17bcafcbc8be50bb022d629f6bc76f2dc7
mingyi.wu <15801227644@126.com>
2020-05-09 18:35:14 +0800

    add  middlerware PackTimer.go

---
+++ app/middlerware/PackTimer.go
@@ -0,0 +1,16 @@
+package middlerware
+
+import (
+	"github.com/wmyi/gn/gn"
+)
+
+type PackTimer struct {
+}
+
+func (t *PackTimer) Before(pack gn.IPack) {
+
+}
+
+func (t *PackTimer) After(pack gn.IPack) {
+
+}
